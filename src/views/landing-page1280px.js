744f469ae84c66b0c41b38cc0c4d4d82fb65827a
TW <82594630+TravisWyche@users.noreply.github.com>
2024-02-01 10:40:24 -0700

    Commit from TeleportHQ

--- src/views/landing-page1280px.js
+++ src/views/landing-page1280px.js
@@ -17,70 +17,56 @@ const LandingPage1280px = (props) => {
         <div className="landing-page1280px-texth2">
           <span className="landing-page1280px-text01">
             <span>
-              <span>
-                IS A
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <br></br>
-              <span>REVOLUTION</span>
-              <br></br>
-              <span>in web3 crowdfunding</span>
+              IS A
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
             </span>
+            <br></br>
+            <span>REVOLUTION</span>
+            <br></br>
+            <span>in web3 crowdfunding</span>
           </span>
         </div>
         <div className="landing-page1280px-container1">
           <button className="landing-page1280px-buttonnone">
             <div className="landing-page1280px-textarea">
-              <span className="landing-page1280px-text08">
-                <span>EXPLORE</span>
-              </span>
+              <span className="landing-page1280px-text07">EXPLORE</span>
             </div>
           </button>
           <button className="landing-page1280px-buttonnone1">
             <div className="landing-page1280px-textarea1">
-              <span className="landing-page1280px-text10">
-                <span>LAUNCH</span>
-              </span>
+              <span className="landing-page1280px-text08">LAUNCH</span>
             </div>
           </button>
         </div>
         <div className="landing-page1280px-texth3">
-          <span className="landing-page1280px-text12">
-            <span>
-              <span>Crafting your project should be a blast not a burden</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span>Relax!</span>
-              <br></br>
-              <span>we’ve got u!</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span>
-                focus on what you know &amp; let yeeter handle the rest
-              </span>
-            </span>
+          <span className="landing-page1280px-text09">
+            <span>Crafting your project should be a blast not a burden</span>
+            <br></br>
+            <br></br>
+            <span>Relax!</span>
+            <br></br>
+            <span>we’ve got u!</span>
+            <br></br>
+            <br></br>
+            <span>focus on what you know &amp; let yeeter handle the rest</span>
           </span>
         </div>
         <div className="landing-page1280px-containersection">
           <div className="landing-page1280px-texth4">
-            <span className="landing-page1280px-text25">Brain farts!</span>
+            <span className="landing-page1280px-text19">Brain farts!</span>
           </div>
           <div className="landing-page1280px-container2">
             <div className="landing-page1280px-textp">
-              <span className="landing-page1280px-text26">
-                <span>
-                  With Yeeter, your idea takes center stage. We don’t judge –
-                  this is your creative sanctuary. Here, you can quickly bring
-                  to life any wild, oddball idea, no matter how big or small.
-                  Our platform is your springboard for rallying a community
-                  around your purpose.
-                </span>
+              <span className="landing-page1280px-text20">
+                With Yeeter, your idea takes center stage. We don’t judge – this
+                is your creative sanctuary. Here, you can quickly bring to life
+                any wild, oddball idea, no matter how big or small. Our platform
+                is your springboard for rallying a community around your
+                purpose.
               </span>
             </div>
             <div className="landing-page1280px-image"></div>
@@ -88,65 +74,63 @@ const LandingPage1280px = (props) => {
         </div>
         <div className="landing-page1280px-containersection1">
           <div className="landing-page1280px-texth31">
-            <span className="landing-page1280px-text28">Fucking Yeet!</span>
+            <span className="landing-page1280px-text21">Fucking Yeet!</span>
           </div>
           <div className="landing-page1280px-container3">
             <div className="landing-page1280px-image1"></div>
             <div className="landing-page1280px-textp1">
-              <span className="landing-page1280px-text29">
-                <span>
-                  Embark on a journey where every project spawns its own Moloch
-                  DAO. Every contributor becomes a vital part of the movement,
-                  earning loot shares in the DAO. Project teams and their
-                  audiences are bound by mutual trustlessness – no rugs can be
-                  pulled. Every contribution matters and everyone’s a
-                  stakeholder in your vision.
-                </span>
+              <span className="landing-page1280px-text22">
+                Embark on a journey where every project spawns its own Moloch
+                DAO. Every contributor becomes a vital part of the movement,
+                earning loot shares in the DAO. Project teams and their
+                audiences are bound by mutual trustlessness – no rugs can be
+                pulled. Every contribution matters and everyone’s a stakeholder
+                in your vision.
               </span>
             </div>
           </div>
         </div>
         <div className="landing-page1280px-containersection2">
           <div className="landing-page1280px-texth32">
-            <span className="landing-page1280px-text31">LARP Adulting</span>
+            <span className="landing-page1280px-text23">LARP Adulting</span>
           </div>
           <div className="landing-page1280px-container4">
             <div className="landing-page1280px-textp2">
-              <span className="landing-page1280px-text32">
-                <span>
-                  Yeeter bakes governance right into the mix – think maximum
-                  transparency with zero headaches. It’s all about helping your
-                  community coordinate seamlessly and successfully. No need to
-                  sweat the complexities of governance; we’ve simplified it for
-                  you. With Yeeter, steering your project’s course is clear,
-                  straightforward, and effective, proving that effective
-                  governance doesn’t have to be hard.
-                </span>
+              <span className="landing-page1280px-text24">
+                Yeeter bakes governance right into the mix – think maximum
+                transparency with zero headaches. It’s all about helping your
+                community coordinate seamlessly and successfully. No need to
+                sweat the complexities of governance; we’ve simplified it for
+                you. With Yeeter, steering your project’s course is clear,
+                straightforward, and effective, proving that effective
+                governance doesn’t have to be hard.
               </span>
             </div>
             <div className="landing-page1280px-image2"></div>
           </div>
         </div>
         <div className="landing-page1280px-texth33">
-          <span className="landing-page1280px-text34">
-            Here’s the deal: We fucking love you. We’re hard-core cultists of
-            purpose-driven communities. Yeeter is where passion meets action.
+          <span className="landing-page1280px-text25">
+            <span>Here’s the deal: We fucking love you</span>
+            <br></br>
+            <span>
+              We’re hard-core cultists of purpose-driven communities Yeeter is
+              where passion meets action
+            </span>
           </span>
         </div>
         <div className="landing-page1280px-footer">
           <div className="landing-page1280px-link">
-            <span className="landing-page1280px-text35">tell us things</span>
+            <span className="landing-page1280px-text29">tell us things</span>
           </div>
           <div className="landing-page1280px-link1">
-            <span className="landing-page1280px-text36">
-              <span>be our friend</span>
-            </span>
+            <span className="landing-page1280px-text30">be our friend</span>
           </div>
           <div className="landing-page1280px-link2">
-            <span className="landing-page1280px-text38">give us money</span>
+            <span className="landing-page1280px-text31">give us money</span>
           </div>
           <div className="landing-page1280px-link3">
-            <span className="landing-page1280px-text39">read our thots</span>
+            <span className="landing-page1280px-text32">read our thots</span>
           </div>
         </div>
       </div>
